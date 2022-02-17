f572495d82a7941dc617ba2a5afd7337c3b8f8ff
eXtreme PC eXtreme PC@DESKTOP-9ADPGTT(Karla Fallas) <>
2022-02-16 20:55:26 -0600

    Primer Commit Tarea02

---
+++ src/main/java/com/AdoptNowCR/AdoptNowCrApplication.java
@@ -0,0 +1,13 @@
+package com.AdoptNowCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptNowCrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptNowCrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AdoptNowCR/AdoptNowCrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdoptNowCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptNowCrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
